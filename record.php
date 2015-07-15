154d4fead77c14e3239d854914f9904c179da5c4
Jonathan Rich <jdrich@gmail.com>
2015-07-15 19:21:14 -0400

    Adding result output functionality.

--- record.php
+++ record.php
@@ -1,20 +1,46 @@
 <?php
 
+$day = 24 * 60 * 60;
+
 $is = fopen('php://input', 'r');
 
 $post = stream_get_contents($is);
 
 fclose($is);
 
-$record = json_decode($post, true);
-$record['time'] = time();
+if(strlen($post)) {
+    $record = json_decode($post, true);
+    $record['time'] = time();
 
-json_encode($record);
+    json_encode($record);
 
-$record_filename = 'records' . DIRECTORY_SEPARATOR . date('Ymd');
+    $record_filename = 'records' . DIRECTORY_SEPARATOR . date('Ymd');
 
-$fs = fopen($record_filename, 'a');
+    $fs = fopen($record_filename, 'a');
 
-fputs($fs, json_encode($record) . "\n");
+    fputs($fs, json_encode($record) . "\n");
 
-fclose($fs);
+    fclose($fs);
+} else {
+    $start = isset($_GET['start']) ? $_GET['start'] : strtotime('last sunday');
+    $end = isset($_GET['end']) ? $_GET['end'] : strtotime('sunday');
+
+    $records = [];
+
+    while($start <= $end) {
+        $record_filename = 'records' . DIRECTORY_SEPARATOR . date('Ymd', $start);
+
+        if(file_exists($record_filename)) {
+            $data = explode("\n", file_get_contents($record_filename));
+
+            foreach($data as $datum) {
+                $records[] = $datum;
+            }
+        }
+
+        $start += $day;
+    }
+
+    header('Content-Type: application/json');
+    echo '[' . implode(',', $records) . ']';
+}
